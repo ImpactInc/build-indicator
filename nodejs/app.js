515effb9f4b9473e1d10609acbda403dbda01ebe
Joseph Rautenbach <joseph@oulap.com>
2018-06-18 10:34:51 +0200

    Formatting

--- nodejs/app.js
+++ nodejs/app.js
@@ -1,5 +1,4 @@
 // Using the feed-watcher package
-
 var Watcher = require('feed-watcher');
 
 var fs = require('fs');
@@ -9,21 +8,21 @@ var config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
 //console.log(config);
 
 // Basic authentication for testing
-var url = config.url.replace('://', '://'+encodeURIComponent(config.user)+':'+encodeURIComponent(config.pass)+'@');
+var url = config.url.replace('://', '://' + encodeURIComponent(config.user) + ':' + encodeURIComponent(config.pass) + '@');
 
 var watcher = new Watcher(url, config.watchInterval);
 
 // Upon a new item in the feed
 watcher.on('new entries', function(entries) {
 	// Notify for each new item
-    entries.forEach(entry => console.log('New entry at ' + entry.pubdate + ': ' + entry.title));
+	entries.forEach(entry => console.log('New entry at ' + entry.pubdate + ': ' + entry.title));
 })
 
 // Start watching the feed
 watcher
-    .start()
-    .then(entries => console.log('Got entries, count: ' + entries.length))
-    .catch(err => console.error(err));
+	.start()
+	.then(entries => console.log('Got entries, count: ' + entries.length))
+	.catch(err => console.error(err));
 
 // Stop watching the feed
 //watcher.stop();
